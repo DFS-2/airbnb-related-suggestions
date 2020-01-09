c2d3c05eb1c3ff10e876c23b314661b5b65e1790
Divyanka Salona <devisalona@gmail.com>
2020-01-08 16:30:28 -0800

    Change config requirement in db

--- database/index.js
+++ database/index.js
@@ -1,4 +1,4 @@
-const config = require ('../config/');
+// const config = require ('../config/');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 const Promise = require('bluebird');
